afa3d317ed6c7f28c2b69a0d3229234af5fc2b27
Ryan Durso <rdurso86@gmail.com>
2020-04-06 19:00:28 -0400

    changed folder structure

--- public/js/addNewScript.js
+++ public/js/addNewScript.js
@@ -1,7 +1,10 @@
   $(document).ready(function(){
-    $('.collapsible').collapsible();
-    $('input#input_text, textarea#textarea2').characterCounter();
-    $('select').formSelect();
+
+    M.AutoInit();
+
+    // $('.collapsible').collapsible();
+    // $('input#input_text, textarea#textarea2').characterCounter();
+    // $('select').formSelect();
     //can not get the all of these to work
     //select is for drop down selection of states that is unused currently
     });
