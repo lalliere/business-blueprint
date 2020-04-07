c954a1ef44544dac4f3181e26754b4ca39ba12f6
Ryan Durso <rdurso86@gmail.com>
2020-04-06 20:01:52 -0400

    state dropdown works now, changed script to autoinit

--- public/js/addNewScript.js
+++ public/js/addNewScript.js
@@ -1,13 +1,11 @@
-  $(document).ready(function(){
-
-    M.AutoInit();
-
-    // $('.collapsible').collapsible();
-    // $('input#input_text, textarea#textarea2').characterCounter();
-    // $('select').formSelect();
-    //can not get the all of these to work
-    //select is for drop down selection of states that is unused currently
-    });
+M.AutoInit();
+  //$(document).ready(function(){
+  //$('.collapsible').collapsible();
+  //$('input#input_text, textarea#textarea2').characterCounter();
+  // $('select').formSelect();
+  //can not get the all of these to work
+  //select is for drop down selection of states that is unused currently
+  //});
   
 
         
