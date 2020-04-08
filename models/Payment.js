a91b2e313482d9e18b7bc7a2c71be340aab50a39
Anastasia Satysheva <anastasia.satysheva@gmail.com>
2020-04-08 01:08:26 -0400

    Fixed lint errors and changed structure of api-invoice.js.

--- models/Payment.js
+++ models/Payment.js
@@ -1,13 +1,13 @@
 module.exports = function(sequelize, DataTypes) {
-    const Payment = sequelize.define("Payment", {
-        invoice_id: {
-            type:DataTypes.INTEGER,
-            allowNull: false
-          },
-          amount: {
-            type:DataTypes.DECIMAL(10,2),
-            allowNull: false
-          },
-    });
-    return Payment;
-  };
+  const Payment = sequelize.define("Payment", {
+    invoice_id: {
+      type:DataTypes.INTEGER,
+      allowNull: false
+    },
+    amount: {
+      type:DataTypes.DECIMAL(10,2),
+      allowNull: false
+    },
+  });
+  return Payment;
+};
--- models/order.js
+++ models/order.js
@@ -1,17 +1,17 @@
 module.exports = function(sequelize, DataTypes) {
   const Order = sequelize.define("Order", {
-      customer_id: {
-          type:DataTypes.INTEGER,
-          allowNull: false
-        },
-        description: {
-          type:DataTypes.STRING,
-          allowNull: false
-        },
-        amount: {
-          type:DataTypes.DECIMAL(10,2),
-          allowNull: false
-        },
+    customer_id: {
+      type:DataTypes.INTEGER,
+      allowNull: false
+    },
+    description: {
+      type:DataTypes.STRING,
+      allowNull: false
+    },
+    amount: {
+      type:DataTypes.DECIMAL(10,2),
+      allowNull: false
+    },
   });
   return Order;
 };
