7ac795fb9a728d3d7484be8c835d4f8b7c4a4595
HannahYudkin <hannahyudkin@gmail.com>
2020-04-11 01:05:47 -0400

    Completed routes.

--- routes/customer-routes.js
+++ routes/customer-routes.js
@@ -4,16 +4,37 @@ const { Router } = require("express");
 
 const customer = Router();
 
-//Tested
+//WORKS
 //get a specific customer
 customer.post("/customers/updatedelete", function(req, res) {
   db.Customer.findAll({
     where: {
-      first_name: req.body.first_name,
-      last_name: req.body.last_name
+      first_name: req.body.udCustFirst,
+      last_name: req.body.udCustLast
     }
   }).then(function(dbCustomer) {
     res.json(dbCustomer);
+  });
+});
+
+//WORKS
+//update a specific customer
+customer.put("/customers/update/:id", function(req, res) {
+  console.log(req.params.id);
+  let str = req.body.uCustPhone;
+  let newStr = str.replace(/-/g, "");
+  db.Customer.update({
+    first_name: req.body.uCustFirst,
+    last_name: req.body.uCustLast,
+    email: req.body.uCustEmail,
+    phone_number: newStr
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function() {
+    //res.json(dbCustomer);
+    res.send(200);
   });
 });
 
@@ -29,8 +50,6 @@ customer.post("/customers/", function(req, res) {
     res.json(dbCustomer);
   });
 });
-
-
 
 //get a specific customer by id
 customer.get("/customers/:id", function(req, res) {
@@ -60,10 +79,9 @@ customer.post("/customer/addNew", function(req, res) {
   });
 });
 
-
 // DELETE route for deleting posts
-customer.delete("/customer/posts/:id", function(req, res) {
-  db.Post.destroy({
+customer.delete("/customers/delete/:id", function(req, res) {
+  db.Customer.destroy({
     where: {
       id: req.params.id
     }
