23a9dafb8cd0c8c1848fcde23be83aaac7bab9a2
HannahYudkin <hannahyudkin@gmail.com>
2020-04-11 08:43:21 -0400

    Fixed info display.

--- public/js/search.js
+++ public/js/search.js
@@ -1,24 +1,31 @@
 M.AutoInit();
 
 $("#search-form").submit(function(event) {
-    event.preventDefault();
-    var $inputs = $("#search-form :input");
-    console.log($inputs);
-  
-    // get an associative array of just the values.
-    var values = {};
-    $inputs.each(function() {
-      values[this.id] = $(this).val();
-    });
-    console.log(values);
+  event.preventDefault();
+  var $inputs = $("#search-form :input");
+  console.log($inputs);
 
-    $.ajax({
-        url: "/customers/",
-        method: "POST",
-        data: values
-      }).then(function(response) {
-        console.log(response);
-        $('#resultsBody').text(JSON.stringify(response));
-      });
+  // get an associative array of just the values.
+  var values = {};
+  $inputs.each(function() {
+    values[this.id] = $(this).val();
+  });
+  console.log(values);
 
-})
+  $.ajax({
+    url: "/customers/",
+    method: "POST",
+    data: values
+  }).then(function(response) {
+    console.log(response);
+    $("#resultsBody").text(
+      JSON.parse(JSON.stringify(response[0].first_name)) +
+        " " +
+        JSON.parse(JSON.stringify(response[0].last_name)) +
+        ": " +
+        JSON.parse(JSON.stringify(response[0].email)) +
+        " " +
+        JSON.parse(JSON.stringify(response[0].phone_number))
+    );
+  });
+});
